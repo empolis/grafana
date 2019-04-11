3302916199fe10fdcb9314c146737de91e6dd511
Jürgen Kreileder <juergen.kreileder@empolis.com>
2019-04-11 23:54:38 +0200

    Empolis branding

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 ﻿import React, { FC } from 'react';
-import { Tooltip } from '@grafana/ui';
 
 interface Props {
   appName: string;
@@ -16,37 +15,13 @@ export const Footer: FC<Props> = React.memo(
         <div className="text-center">
           <ul>
             <li>
-              <a href="http://docs.grafana.org" target="_blank">
-                <i className="fa fa-file-code-o" /> Docs
-              </a>
-            </li>
-            <li>
-              <a href="https://grafana.com/services/support" target="_blank">
-                <i className="fa fa-support" /> Support Plans
-              </a>
-            </li>
-            <li>
-              <a href="https://community.grafana.com/" target="_blank">
-                <i className="fa fa-comments-o" /> Community
-              </a>
-            </li>
-            <li>
-              <a href="https://grafana.com" target="_blank">
+              <a href="https://empolis.com" target="_blank">
                 {appName}
               </a>{' '}
               <span>
                 v{buildVersion} (commit: {buildCommit})
               </span>
             </li>
-            {newGrafanaVersionExists && (
-              <li>
-                <Tooltip placement="auto" content={newGrafanaVersion}>
-                  <a href="https://grafana.com/get" target="_blank">
-                    New version available!
-                  </a>
-                </Tooltip>
-              </li>
-            )}
           </ul>
         </div>
       </footer>
